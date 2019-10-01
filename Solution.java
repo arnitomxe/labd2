e3ac017ecb7661b5a7903db846dd397bbe499beb
arnitomxe <55348250+arnitomxe@users.noreply.github.com>
2019-10-01 21:24:08 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,13 +5,13 @@ public class Solution {
 	static boolean palindrome(String s) {
 		// TODO
 		//System.out.println("test: "+s.length());
-		if(s.length()<=1)//stop rekursiju, ja varda garums ir parak mazs
+		if(s.length()<=1)//aptur rekursiju, ja varda garums ir parak mazs
 			return true;
 		else
 			if(s.toLowerCase().substring(0, 1).equals(s.toLowerCase().substring(s.length()-1)))//parbauda pirmo simbolu ar pedejo ignorejot Case
 				return palindrome(s.substring(1, s.length()-1));//nodod rekursijai vardu, nonemot pirmo un pedejo simbolu
 			else
-				return false;
+				return false;//aptur rekursiju, ja vards nav palindroms
 	}
 	
 	static long superDigit(long x) {
@@ -24,7 +24,7 @@ public class Solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(palindrome("abcba")); //true
-		System.out.println(palindrome("Sapalsarîtadçdatîraslapas")); //true
+		System.out.println(palindrome("SapalsarÃ®tadÃ§datÃ®raslapas")); //true
 		System.out.println(palindrome("stirna")); //false
 		System.out.println(superDigit(9875)); //29
 		System.out.println(superDigit(2)); //2
